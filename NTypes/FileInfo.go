d5c311efea04c76066a22763483a41819333947c
Christopher Stelly <cdstelly@gmail.com>
2017-09-22 10:11:45 -0500

    fixed possible out of bounds bug

--- NTypes/FileInfo.go
+++ NTypes/FileInfo.go
@@ -45,21 +45,17 @@ func (cbo ByOffset) Less(i, j int) bool {
 	return cbo[i].Clusteroffset < cbo[j].Clusteroffset
 }
 
-// FileInfo implements sort interface
+// Sorting interfaces
 type ByCtime []FileInfo
-
 func (a ByCtime) Len() int           { return len(a) }
 func (a ByCtime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByCtime) Less(i, j int) bool { return a[i].Createtime.Before(a[j].Createtime) }
 
-
-// FileInfo implements sort interface
 type ByFilename []FileInfo
-
 func (a ByFilename) Len() int           { return len(a) }
 func (a ByFilename) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByFilename) Less(i, j int) bool {
-	if a[i].Filenames == nil { return false }
-	if a[j].Filenames == nil { return false }
+	if len(a[i].Filenames) == 0 { return false }
+	if len(a[j].Filenames) == 0 { return false }
 	return strings.Compare(a[i].Filenames[0], a[j].Filenames[0]) == -1
 }
