2b4f462f75666eb99c15050774c359c2e4ef6c18
Christopher Stelly <cdstelly@gmail.com>
2017-09-23 22:57:10 -0500

    implement union - fixed several bugs

---
+++ NActions/UnionAction.go
@@ -0,0 +1,73 @@
+package NActions
+
+import "fmt"
+import (
+	"../NTypes"
+	"reflect"
+)
+
+type UnionAction struct {
+	executed  bool
+	dependsOn BaseAction
+	filters []NTypes.Filter
+
+	results []string
+	VariableList []string
+}
+
+func (na *UnionAction) BeenExecuted() bool {
+	return na.executed
+}
+
+func (na *UnionAction) DependencySatisfied() bool {
+	if na.dependsOn == nil {
+		return true
+	}
+	return na.dependsOn.BeenExecuted()
+}
+
+func (na *UnionAction) SetDependency(action BaseAction) {
+	na.dependsOn = action
+}
+func (na *UnionAction) Execute() {
+	if na.dependsOn != nil {
+		fmt.Println("union has a dependency which hasn't been met..")
+		if na.dependsOn.BeenExecuted() == false {
+			na.dependsOn.Execute()
+		}
+	}
+	fmt.Println("going to execute union..")
+	resultSet := []string{}
+
+	operateOn := na.dependsOn.GetResults()
+
+	//todo what about sha1 and sha256..
+	//todo optimize this n^2 comparison loop..
+	if strList, ok := operateOn.([]NTypes.MD5); ok {
+		fmt.Println("going to execute union against MD5..")
+		for _, stringFromVar := range na.VariableList {
+			for _, stringFromDep := range strList {
+				//fmt.Println("Comparing: ", stringFromVar, stringFromDep)
+				if stringFromVar == stringFromDep.Digest {
+					resultSet = append(resultSet,stringFromDep.Digest)
+				}
+			}
+		}
+	}
+
+	na.results = resultSet
+	na.executed = true
+}
+
+func (na *UnionAction) GetResults() interface{}{
+	if !na.BeenExecuted() {
+		na.Execute()
+	}
+	return na.results
+}
+
+func (na *UnionAction) SetFilters(filters []NTypes.Filter) {
+	//TODO: investigate if resetting executed status will be a problem:
+	na.executed = false
+	na.filters = filters
+}
