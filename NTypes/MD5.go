2997055fb4dab138918af892ec94da4525845509
Christopher Stelly <cdstelly@gmail.com>
2017-11-02 20:38:03 -0500

    sample json implementation

--- NTypes/MD5.go
+++ NTypes/MD5.go
@@ -1,5 +1,10 @@
 package NTypes
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type MD5 struct {
 	Digest string
 	HashOf interface{}
@@ -7,4 +12,12 @@ type MD5 struct {
 
 func (m MD5) String() string {
 	return string(m.Digest)
+}
+
+func (m MD5) JSON() string {
+	jsonEncoding, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	return string(jsonEncoding)
 }
