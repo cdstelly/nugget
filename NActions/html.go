ac6fd4c8e94f8b6e46b9fb327021c877066cc102
Christopher Stelly <cdstelly@gmail.com>
2017-11-10 13:43:02 -0600

    adding html type

---
+++ NActions/html.go
@@ -0,0 +1,67 @@
+package NActions
+
+import "fmt"
+import (
+	"../NTypes"
+	"crypto/md5"
+)
+
+type HTMLAction struct {
+	executed  bool
+	dependsOn BaseAction
+	filters []NTypes.Filter
+
+	results []NTypes.HTML
+}
+
+func (na *HTMLAction) BeenExecuted() bool {
+	return na.executed
+}
+
+func (na *HTMLAction) DependencySatisfied() bool {
+	if na.dependsOn == nil {
+		return true
+	}
+	return na.dependsOn.BeenExecuted()
+}
+
+func (na *HTMLAction) SetDependency(action BaseAction) {
+	na.dependsOn = action
+}
+func (na *HTMLAction) Execute() {
+	if na.dependsOn != nil {
+		//fmt.Println("md5 has a dependency which hasn't been met..")
+		if na.dependsOn.BeenExecuted() == false {
+
+			na.dependsOn.Execute()
+		}
+	}
+	//fmt.Println("going to execute md5..")
+
+	operateOn := na.dependsOn.GetResults()
+	if _, ok := operateOn.([]NTypes.FileInfo); ok {
+		var files []NTypes.FileInfo
+		files = operateOn.([]NTypes.FileInfo)
+		for _,file := range files {
+			hasher := md5.New()
+			hasher.Write(file.GetFileData())
+			myhash := fmt.Sprintf("%x", hasher.Sum(nil))
+			//fmt.Println("fn:" + file.Filenames[0] + "\tmd5: " + myhash)
+			na.results = append(na.results, NTypes.MD5{Digest:myhash, HashOf:file})
+		}
+	}
+	na.executed = true
+}
+
+func (na *HTMLAction) GetResults() interface{}{
+	if !na.BeenExecuted() {
+		na.Execute()
+	}
+	return na.results
+}
+
+func (na *HTMLAction) SetFilters(filters []NTypes.Filter) {
+	//TODO: investigate if resetting executed status will be a problem:
+	na.executed = false
+	na.filters = filters
+}
