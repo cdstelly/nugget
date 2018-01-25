66ba853ef094bd50cadecec9b99d9e05620054df
Christopher Stelly <cdstelly@gmail.com>
2018-01-24 21:29:34 -0600

    fix interactive

--- src/github.com/cdstelly/nugget/nugget.go
+++ src/github.com/cdstelly/nugget/nugget.go
@@ -14,6 +14,7 @@ import (
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"errors"
 	"strings"
+	"os/signal"
 )
 
 var (
@@ -44,6 +45,8 @@ func init() {
 	//hold a string->nugType values
 	typeRegistry = make(map[string]reflect.Type)
 	setupTypeRegstry()
+
+	CatchTerm()
 }
 
 func flagCheck() bool {
@@ -629,4 +632,14 @@ func main() {
 			}
 		}
 	}
+}
+
+func CatchTerm() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func(){
+		for sig := range c {
+			fmt.Println(sig.String())
+		}
+	}()
 }
