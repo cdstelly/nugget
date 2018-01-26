e74842da395efb97ef6bc4c858dc8afcea95c455
Christopher Stelly <cdstelly@gmail.com>
2018-01-25 21:50:26 -0600

    fix interactive

--- src/github.com/cdstelly/nugget/expressions/extractors/ExtractNTFS.go
+++ src/github.com/cdstelly/nugget/expressions/extractors/ExtractNTFS.go
@@ -105,7 +105,7 @@ crtime
 }
 
 func (na *ExtractNTFS) UploadData() {
-	client, err := rpc.DialHTTP("tcp", "192.168.1.198:2001")
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:2001")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
